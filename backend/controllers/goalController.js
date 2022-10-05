55b3e6769f87ab8ac0977bb90df9a99ee7f823cd
נסטיה טרנובסקי <nastyaternovski@h-MBP-sl-nstyh.lan>
2022-10-05 16:57:28 +0300

    Prepare deploy

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,7 +9,7 @@ const User = require('../models/userModel')
 //@access   Private
 const getGoals = asyncHandler(async (req, res) =>{
     const goals = await Goal.find({ user: req.user.id })
-    res.status(200).json({goals})
+    res.status(200).json(goals)
 })
 
 //@desc     Set goals
@@ -25,7 +25,7 @@ const setGoal = asyncHandler(async (req, res) =>{
         text: req.body.text,
         user: req.user.id
     })
-    res.status(200).json({goal})
+    res.status(200).json(goal)
 })
 
 //@desc     Update goals
@@ -38,23 +38,21 @@ const updateGoal = asyncHandler(async (req, res) =>{
         throw new Error('Goal not found')
     }
 
-    const user = await User.findById(req.user.id)
-
     // Check for user
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error('User not found')
     }
 
     // Make sure the looged in user matches the goal user
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized') 
     }
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
 
-    res.status(200).json({updatedGoal})
+    res.status(200).json(updatedGoal)
 })
 
 //@desc     Delete goals
@@ -67,16 +65,14 @@ const deleteGoal = asyncHandler(async (req, res) =>{
         throw new Error('Goal not found')
     }
 
-    const user = await User.findById(req.user.id)
-
     // Check for user
-    if(!user){
+    if(!req.user){
         res.status(401)
         throw new Error('User not found')
     }
 
     // Make sure the looged in user matches the goal user
-    if(goal.user.toString() !== user.id){
+    if(goal.user.toString() !== req.user.id){
         res.status(401)
         throw new Error('User not authorized') 
     }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import {useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useSelector, useDispatch} from 'react-redux'
+import GoalForm from '../componenets/GoalForm'
+import Spinner from '../componenets/Spinner'
+import { getGoals, reset } from '../features/goals/goalSlice'
+import GoalItem from '../componenets/GoalItem'
 
 function Dashboard() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const {user} = useSelector((state) => state.auth)
+  const { goals, isLoading, isError, message } = useSelector((state) => state.goals)
+
+  useEffect(() => {
+    if(isError){
+      console.log(message)
+    }
+    if(!user){
+      navigate('/login')
+    }
+
+    //dispatch goals from the backend and put it in goals variable
+    dispatch(getGoals())
+
+    //when we leave the deshboard i want the goals to clear
+    //if we want to do something when the component unmounts we need to return fron the useEffect
+    return () => {
+      dispatch(reset())
+    }
+
+  },[user, navigate, isError, message, dispatch])
+
+  if(isLoading) {
+    return <Spinner />
+  }
+  
   return (
-    <div>Dashboard</div>
+  <>
+    <section className='heading'>
+      <h1>Welcome { user && user.name } </h1>
+      <p>Goals Dasboard</p>
+    </section>
+
+    <GoalForm />
+
+    <section className="content">
+      {goals.length > 0 ? (
+        <div className="goals">
+          {goals.map((goal) => (
+            <GoalItem key={goal._id} goal={goal} />
+          ))}
+        </div>
+      ) : (
+        <h3> You have not set any goals</h3>
+      )}
+    </section>
+    </>
   )
 }
 
