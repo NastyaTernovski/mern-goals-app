88b9bb3dbc76bae265bf67f59caac107d52b8af1
נסטיה טרנובסקי <nastyaternovski@h-MBP-sl-nstyh.lan>
2022-10-08 15:25:17 +0300

    start repository

--- backend/server.js
+++ backend/server.js
@@ -17,18 +17,21 @@ app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
 // Serve frontend
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+if(process.env.NODE_ENV == 'production') {
+    console.log('here')
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-    app.get('*', (req, res) =>
-     res.sendFile(
-         path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-        )
-    )
-} else {
-    app.get('/', (req, res) => res.send('Plese set to production'))
-}
-
-app.use(errorHandler)
+    app.get('*', (req, res) => {
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    }
+  )  
+  
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
+  
+  app.use(errorHandler);
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`))
